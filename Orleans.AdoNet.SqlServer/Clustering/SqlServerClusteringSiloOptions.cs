fd79ddc8364ca0821d014414738821a518e8d3c5
Shengjie Yan <ysjshengjie@live.cn>
2019-11-03 21:10:46 +0800

    enable set

--- Orleans.AdoNet.SqlServer/Clustering/SqlServerClusteringSiloOptions.cs
+++ Orleans.AdoNet.SqlServer/Clustering/SqlServerClusteringSiloOptions.cs
@@ -10,6 +10,6 @@ namespace Orleans.AdoNet.SqlServer.Clustering
         /// <summary>
         /// The invariant name of the connector for membership's database.
         /// </summary>
-        public new string Invariant { get; private set; }= AdoNetInvariants.InvariantNameSqlServer;
+        public new string Invariant { get; set; }= AdoNetInvariants.InvariantNameSqlServer;
     }
 }
--- Orleans.AdoNet.SqlServer/Persistence/SqlServerGrainStorageOptions.cs
+++ Orleans.AdoNet.SqlServer/Persistence/SqlServerGrainStorageOptions.cs
@@ -8,6 +8,6 @@ namespace Orleans.AdoNet.SqlServer.Persistence
         /// <summary>
         /// The invariant name of the connector for membership's database.
         /// </summary>
-        public new string Invariant { get; private set; }= AdoNetInvariants.InvariantNameSqlServer;
+        public new string Invariant { get; set; }= AdoNetInvariants.InvariantNameSqlServer;
     }
 }
