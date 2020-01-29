68b3f92bb5662d5d09cb77898fe7fb53c98a174e
Shengjie Yan <ysjshengjie@live.cn>
2020-01-29 11:17:11 +0800

    extract method to build options

--- Orleans.Extensions/AdoNet/Persistence/AdoNetStorageExtensions.cs
+++ Orleans.Extensions/AdoNet/Persistence/AdoNetStorageExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using Orleans.Configuration;
 using Orleans.Hosting;
 
@@ -16,7 +15,12 @@ namespace Orleans.AdoNet.Persistence
         /// </remarks>
         public static ISiloHostBuilder AddOrleansAdoNetGrainStorageAsDefault<T>(
           this ISiloHostBuilder builder,
-          Action<T> configureOptions) where T : AdoNetGrainStorageOptions => builder.AddAdoNetGrainStorageAsDefault(configureOptions as Action<AdoNetGrainStorageOptions>);
+          Action<T> configureOptions) where T : AdoNetGrainStorageOptions, new()
+        {
+            var newAction = new Action<AdoNetGrainStorageOptions>(options => ConvertToAdoNetGrainStorageOptions(configureOptions, options));
+
+            return builder.AddAdoNetGrainStorageAsDefault(newAction);
+        }
 
 
         /// <summary>
@@ -28,8 +32,28 @@ namespace Orleans.AdoNet.Persistence
         public static ISiloHostBuilder AddOrleansAdoNetGrainStorage<T>(
             this ISiloHostBuilder builder,
             string name,
-            Action<T> configureOptions) where T : AdoNetGrainStorageOptions
-            => builder.AddAdoNetGrainStorage(name, configureOptions as Action<AdoNetGrainStorageOptions>);
+            Action<T> configureOptions) where T : AdoNetGrainStorageOptions, new()
+        {
+            var newAction = new Action<AdoNetGrainStorageOptions>(options => ConvertToAdoNetGrainStorageOptions(configureOptions, options));
+
+            return builder.AddAdoNetGrainStorage(name, newAction);
+        }
+
+        private static void ConvertToAdoNetGrainStorageOptions<T>(Action<T> configureOptions, AdoNetGrainStorageOptions options)
+            where T : AdoNetGrainStorageOptions, new()
+        {
+            var t = new T();
+            configureOptions(t);
+            options.Invariant = t.Invariant;
+            options.ConnectionString = t.ConnectionString;
+            options.ConfigureJsonSerializerSettings = t.ConfigureJsonSerializerSettings;
+            options.IndentJson = t.IndentJson;
+            options.InitStage = t.InitStage;
+            options.TypeNameHandling = t.TypeNameHandling;
+            options.UseFullAssemblyNames = t.UseFullAssemblyNames;
+            options.UseJsonFormat = t.UseJsonFormat;
+            options.UseXmlFormat = t.UseXmlFormat;
+        }
 
         /// <summary>
         /// Configure silo to use AdoNet grain storage as the default grain storage. Instructions on configuring your database are available at <see href="http://aka.ms/orleans-sql-scripts" />.
@@ -38,9 +62,14 @@ namespace Orleans.AdoNet.Persistence
         /// Instructions on configuring your database are available at <see href="http://aka.ms/orleans-sql-scripts" />.
         /// </remarks>
         public static ISiloBuilder AddOrleansAdoNetGrainStorageAsDefault<T>(
-            this ISiloBuilder builder,
-            Action<T> configureOptions)
-            where T : AdoNetGrainStorageOptions => builder.AddAdoNetGrainStorageAsDefault(configureOptions as Action<AdoNetGrainStorageOptions>);
+        this ISiloBuilder builder,
+        Action<T> configureOptions)
+        where T : AdoNetGrainStorageOptions, new()
+        {
+            var newAction = new Action<AdoNetGrainStorageOptions>(options => ConvertToAdoNetGrainStorageOptions(configureOptions, options));
+
+            return builder.AddAdoNetGrainStorageAsDefault(newAction);
+        }
 
         /// <summary>
         /// Configure silo to use  AdoNet grain storage for grain storage. Instructions on configuring your database are available at <see href="http://aka.ms/orleans-sql-scripts" />.
@@ -52,7 +81,12 @@ namespace Orleans.AdoNet.Persistence
             this ISiloBuilder builder,
             string name,
             Action<T> configureOptions)
-            where T : AdoNetGrainStorageOptions => builder.AddAdoNetGrainStorage(name, configureOptions as Action<AdoNetGrainStorageOptions>);
+            where T : AdoNetGrainStorageOptions, new()
+        {
+            var newAction = new Action<AdoNetGrainStorageOptions>(options => ConvertToAdoNetGrainStorageOptions(configureOptions, options));
+
+            return builder.AddAdoNetGrainStorage(name, newAction);
+        }
 
 
         /// <summary>
@@ -64,7 +98,12 @@ namespace Orleans.AdoNet.Persistence
         public static IServiceCollection AddOrleansAdoNetGrainStorage<T>(
             this IServiceCollection services,
             Action<T> configureOptions)
-            where T : AdoNetGrainStorageOptions => services.AddAdoNetGrainStorage(configureOptions as Action<AdoNetGrainStorageOptions>);
+            where T : AdoNetGrainStorageOptions, new()
+        {
+            var newAction = new Action<AdoNetGrainStorageOptions>(options => ConvertToAdoNetGrainStorageOptions(configureOptions, options));
+
+            return services.AddAdoNetGrainStorage(newAction);
+        }
 
         /// <summary>
         /// Configure silo to use AdoNet grain storage for grain storage. Instructions on configuring your database are available at <see href="http://aka.ms/orleans-sql-scripts" />.
@@ -76,10 +115,11 @@ namespace Orleans.AdoNet.Persistence
           this IServiceCollection services,
           string name,
           Action<T> configureOptions)
-        where T : AdoNetGrainStorageOptions => services.AddAdoNetGrainStorage(name, configureOptions as Action<AdoNetGrainStorageOptions>);
+        where T : AdoNetGrainStorageOptions, new()
+        {
+            var newAction = new Action<AdoNetGrainStorageOptions>(options => ConvertToAdoNetGrainStorageOptions(configureOptions, options));
 
-
+            return services.AddAdoNetGrainStorage(name, newAction);
+        }
     }
-
-
 }
--- Orleans.Extensions/AdoNet/Reminder/AdoNetReminderExtensions.cs
+++ Orleans.Extensions/AdoNet/Reminder/AdoNetReminderExtensions.cs
@@ -21,8 +21,13 @@ namespace Orleans.AdoNet.Reminder
         /// Instructions on configuring your database are available at <see href="http://aka.ms/orleans-sql-scripts" />.
         /// </remarks>
         public static ISiloHostBuilder UseOrleansAdoNetReminderService<T>(
-          this ISiloHostBuilder builder,
-          Action<T> configureOptions) where T : AdoNetReminderTableOptions => builder.UseAdoNetReminderService(configureOptions as Action<AdoNetReminderTableOptions>);
+            this ISiloHostBuilder builder,
+            Action<T> configureOptions) where T : AdoNetReminderTableOptions, new()
+        {
+            var newAction = new Action<AdoNetReminderTableOptions>(options =>
+                ConvertToAdoNetReminderTableOptions(configureOptions, options));
+            return builder.UseAdoNetReminderService(newAction);
+        }
 
 
 
@@ -39,6 +44,20 @@ namespace Orleans.AdoNet.Reminder
         /// </remarks>
         public static ISiloBuilder UseOrleansAdoNetReminderService<T>(
           this ISiloBuilder builder,
-          Action<T> configureOptions) where T : AdoNetReminderTableOptions => builder.UseAdoNetReminderService(configureOptions as Action<AdoNetReminderTableOptions>);
+          Action<T> configureOptions) where T : AdoNetReminderTableOptions, new()
+        {
+            var newAction = new Action<AdoNetReminderTableOptions>(options =>
+                ConvertToAdoNetReminderTableOptions(configureOptions, options));
+            return builder.UseAdoNetReminderService(newAction);
+        }
+
+        private static void ConvertToAdoNetReminderTableOptions<T>(Action<T> configureOptions, AdoNetReminderTableOptions options)
+            where T : AdoNetReminderTableOptions, new()
+        {
+            var t = new T();
+            configureOptions(t);
+            options.Invariant = t.Invariant;
+            options.ConnectionString = t.ConnectionString;
+        }
     }
 }
